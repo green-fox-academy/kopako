75118e3d0e1fa5c6eab5813e5093b64a387089ba
Kopan Dmytro <kopandima@gmail.com>
2018-12-05 13:39:43 +0100

    Hello User REST App

--- week7/day1 wednesday/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week7/day1 wednesday/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
     @RequestMapping("/greeting")
-    public Greatings greeting() {
-        return new Greatings(1, "Hello World");
+    public Greatings greeting(@RequestParam String name) {
+        return new Greatings(1, "Hello, "+ name+ "!");
     }
 }
