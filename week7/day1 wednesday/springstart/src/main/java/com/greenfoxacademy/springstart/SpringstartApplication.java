f22387628261103e90f117c9c1c3ee97a0f77dd6
Kopan Dmytro <kopandima@gmail.com>
2018-12-05 13:23:36 +0100

    HelloWorld

--- week7/day1 wednesday/springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
+++ week7/day1 wednesday/springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
@@ -2,9 +2,20 @@ package com.greenfoxacademy.springstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class SpringstartApplication {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello () {
+        return "Hello world!";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringstartApplication.class, args);
