c978a44129c4557f33ba87fd246a04cf5e15dfad
Kopan Dmytro <kopandima@gmail.com>
2018-12-12 12:31:10 +0100

    HTMLception

--- week8/day3/bankofsimba/src/main/java/com/seadog/bankofsimba/controllers/BankController.java
+++ week8/day3/bankofsimba/src/main/java/com/seadog/bankofsimba/controllers/BankController.java
@@ -3,7 +3,6 @@ package com.seadog.bankofsimba.controllers;
 import com.seadog.bankofsimba.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +12,11 @@ public class BankController {
     public String show(Model model){
         model.addAttribute("account", new BankAccount("Simba", 2000, "lion"));
         return "index";
+    }
+
+    @RequestMapping("/HTMLception")
+    public String htmlCeption(Model model) {
+        model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+        return "HTMLception";
     }
 }
