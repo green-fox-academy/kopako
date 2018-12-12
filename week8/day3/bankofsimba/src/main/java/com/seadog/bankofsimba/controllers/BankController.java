c1820a9ad03b4f1677df39ad85f8512fd7c8aab4
Kopan Dmytro <kopandima@gmail.com>
2018-12-12 12:55:43 +0100

    Filling multiple Accounts

--- week8/day3/bankofsimba/src/main/java/com/seadog/bankofsimba/controllers/BankController.java
+++ week8/day3/bankofsimba/src/main/java/com/seadog/bankofsimba/controllers/BankController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BankController {
 
@@ -18,5 +21,17 @@ public class BankController {
     public String htmlCeption(Model model) {
         model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
         return "HTMLception";
+    }
+
+    @RequestMapping("/list")
+    public String list(Model model) {
+        List<BankAccount> list = new ArrayList<>();
+        list.add(new BankAccount("Pumba", 5, "warthog"));
+        list.add(new BankAccount("Timon", 2, "meerkat"));
+        list.add(new BankAccount("Mufasa", 9999999, "lion"));
+        list.add(new BankAccount("Scar", 666, "lion"));
+        list.add(new BankAccount("Azizi", 0, "hyenas"));
+        model.addAttribute("list", list);
+        return "list";
     }
 }
