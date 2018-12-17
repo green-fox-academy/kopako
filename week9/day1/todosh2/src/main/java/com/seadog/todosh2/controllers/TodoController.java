c8de57a94fce26d5e272f8ff2b2e6ef747b27184
Kopan Dmytro <kopandima@gmail.com>
2018-12-17 11:30:11 +0100

    Create template

--- week9/day1/todosh2/src/main/java/com/seadog/todosh2/controllers/TodoController.java
+++ week9/day1/todosh2/src/main/java/com/seadog/todosh2/controllers/TodoController.java
@@ -3,6 +3,8 @@ package com.seadog.todosh2.controllers;
 import com.seadog.todosh2.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +15,10 @@ public class TodoController {
     @Autowired
     TodoRepository todoRepository;
 
-    @RequestMapping(value = { "/", "/list" })
-    @ResponseBody()
-    public String list() {
-
-        return "This is my first Todo";
+    @RequestMapping(value = {"/", "/list"})
+    public String list(Model model) {
+        model.addAttribute("list", todoRepository.findAll());
+        return "todolist";
     }
 
 }
